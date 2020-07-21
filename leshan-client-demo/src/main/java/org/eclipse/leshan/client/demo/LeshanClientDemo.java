02b0b47d145747a4ef411b1ed894a3dcedbb2f5f
rikard-sics <rikard.hoglund@ri.se>
2020-07-21 14:39:53 +0200

    #726: Generate OSCORE object and use it for endpoint creation
    Code to implement the points listed here:
    https://github.com/eclipse/leshan/pull/718#issuecomment-521316754
    
    The OSCORE object is generated and filled in the Leshan client demo.
    It will then be used to create a ServerInfo object using the
    ServersInfoExtractor. Next the ServerInfo is used in the
    CaliforniumEndpointsManager to generate endpoints towards servers
    that OSCORE should be used with.
    
    See also issue regarding OSCORE client integration:
    https://github.com/eclipse/leshan/issues/726
    
    Signed-off-by: Rikard Höglund <rikard.hoglund@ri.se>

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
@@ -24,8 +24,6 @@ import static org.eclipse.leshan.core.LwM2mId.*;
 
 import java.io.File;
 import java.net.InetAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -55,7 +53,6 @@ import org.eclipse.californium.cose.AlgorithmID;
 import org.eclipse.californium.cose.CoseException;
 import org.eclipse.californium.elements.Connector;
 import org.eclipse.californium.elements.util.SslContextUtil;
-import org.eclipse.californium.oscore.HashMapCtxDB;
 import org.eclipse.californium.oscore.OSCoreCoapStackFactory;
 import org.eclipse.californium.scandium.DTLSConnector;
 import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
@@ -812,6 +809,12 @@ public class LeshanClientDemo {
                 Oscore oscoreObject = new Oscore(12345, oscoreSettings.masterSecret, oscoreSettings.senderId,
                         oscoreSettings.recipientId, oscoreSettings.aeadAlgorithm, oscoreSettings.hkdfAlgorithm,
                         oscoreSettings.masterSalt);
+                initializer.setInstancesForObject(SECURITY, oscoreOnly(serverURI, 123, oscoreObject.getId()));
+                initializer.setInstancesForObject(OSCORE, oscoreObject);
+                initializer.setInstancesForObject(SERVER, new Server(123, lifetime));
+            } else if (useOSCore) {
+                String clientName = Hex.encodeHexString(endpoint.getBytes());
+                Oscore oscoreObject = new Oscore(12345, "11223344", clientName, "BB"); // Partially hardcoded values
                 initializer.setInstancesForObject(SECURITY, oscoreOnly(serverURI, 123, oscoreObject.getId()));
                 initializer.setInstancesForObject(OSCORE, oscoreObject);
                 initializer.setInstancesForObject(SERVER, new Server(123, lifetime));
