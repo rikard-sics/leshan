8f3740e599463454f53fa832669039163330cae0
rikard-sics <rikard.hoglund@ri.se>
2020-10-15 11:22:29 +0200

    Writing of OSCORE objects to client

--- leshan-server-core/src/main/java/org/eclipse/leshan/server/bootstrap/BootstrapUtil.java
+++ leshan-server-core/src/main/java/org/eclipse/leshan/server/bootstrap/BootstrapUtil.java
@@ -12,6 +12,7 @@
  * 
  * Contributors:
  *     Sierra Wireless - initial API and implementation
+ *     Rikard Höglund (RISE) - additions to support OSCORE
  *******************************************************************************/
 package org.eclipse.leshan.server.bootstrap;
 
@@ -23,6 +24,7 @@ import org.eclipse.leshan.core.node.LwM2mObjectInstance;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.node.LwM2mSingleResource;
 import org.eclipse.leshan.server.bootstrap.BootstrapConfig.ACLConfig;
+import org.eclipse.leshan.server.bootstrap.BootstrapConfig.OscoreObject;
 import org.eclipse.leshan.server.bootstrap.BootstrapConfig.ServerConfig;
 import org.eclipse.leshan.server.bootstrap.BootstrapConfig.ServerSecurity;
 
@@ -86,6 +88,25 @@ public class BootstrapUtil {
             resources.add(LwM2mMultipleResource.newIntegerResource(2, aclConfig.acls));
         if (aclConfig.AccessControlOwner != null)
             resources.add(LwM2mSingleResource.newIntegerResource(3, aclConfig.AccessControlOwner));
+
+        return new LwM2mObjectInstance(instanceId, resources);
+    }
+
+    public static LwM2mObjectInstance convertToOscoreInstance(int instanceId, OscoreObject oscoreConfig) {
+        Collection<LwM2mResource> resources = new ArrayList<>();
+
+        if (oscoreConfig.oscoreMasterSecret != null)
+            resources.add(LwM2mSingleResource.newStringResource(0, oscoreConfig.oscoreMasterSecret));
+        if (oscoreConfig.oscoreSenderId != null)
+            resources.add(LwM2mSingleResource.newStringResource(1, oscoreConfig.oscoreSenderId));
+        if (oscoreConfig.oscoreRecipientId != null)
+            resources.add(LwM2mSingleResource.newStringResource(2, oscoreConfig.oscoreRecipientId));
+        if (oscoreConfig.oscoreAeadAlgorithm != null)
+            resources.add(LwM2mSingleResource.newIntegerResource(3, oscoreConfig.oscoreAeadAlgorithm));
+        if (oscoreConfig.oscoreHmacAlgorithm != null)
+            resources.add(LwM2mSingleResource.newIntegerResource(4, oscoreConfig.oscoreHmacAlgorithm));
+        if (oscoreConfig.oscoreMasterSalt != null)
+            resources.add(LwM2mSingleResource.newStringResource(5, oscoreConfig.oscoreMasterSalt));
 
         return new LwM2mObjectInstance(instanceId, resources);
     }
