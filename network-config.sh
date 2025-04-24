0076ed2bdcd58fe7fb512aedb1c49b7b7cd7218c
Rikard Höglund <rikard.hoglund@ri.se>
2025-04-24 13:24:17 +0200

    Minor updates to scripts

--- network-config.sh
+++ network-config.sh
@@ -1,3 +1,4 @@
+# Typically no need to run this
 ifconfig eth0 192.168.0.101 netmask 255.255.255.0
 ifconfig eth0:0 192.168.0.102 netmask 255.255.255.0
 ifconfig eth0:1 192.168.0.103 netmask 255.255.255.0
