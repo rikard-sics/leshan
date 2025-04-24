7f105a8bbbdf2c555b3ce2a47f8a6cf1d7afc80f
Rikard Höglund <rikard.hoglund@ri.se>
2025-04-24 12:10:27 +0200

    Updated version number and added debug printing for testing

--- leshan/leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
+++ leshan/leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
@@ -295,6 +295,7 @@ public class LeshanClientDemo {
         
         // Startup message
         System.out.println("Starting EDHOC-enabled Client Demo Application");
+        System.out.println("Loading EDHOC client code from: " + org.eclipse.californium.edhoc.EdhocClient.toString());
 
         // Parse arguments
         CommandLine cl;
