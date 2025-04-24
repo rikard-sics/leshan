959c36e197360845b2029b3554f229f76ef50db8
Rikard Höglund <rikard.hoglund@ri.se>
2025-04-24 12:42:30 +0200

    Add script and pom files for development work in Eclipse

---
+++ setup-dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set up the code for development work in Eclipse
+
+cp leshan/leshan-server-redis/pom.dev leshan/leshan-server-redis/pom.xml
+cp leshan/leshan-server-core/pom.dev /leshan-server-core/pom.xml
+cp leshan/pom.dev leshan/pom.xml
+cp leshan/leshan-integration-tests/pom.dev leshan/leshan-integration-tests/pom.xml
+cp leshan/leshan-server-cf/pom.dev leshan/leshan-server-cf/pom.xml
+cp leshan/leshan-bsserver-demo/pom.dev leshan/leshan-bsserver-demo/pom.xml
+cp leshan/leshan-client-core/pom.dev leshan/leshan-client-core/pom.xml
+cp leshan/leshan-client-demo/pom.dev leshan/leshan-client-demo/pom.xml
+cp leshan/leshan-client-cf/pom.dev leshan/leshan-client-cf/pom.xml
+cp leshan/leshan-core/pom.dev leshan/leshan-core/pom.xml
+cp leshan/leshan-core-cf/pom.dev leshan/leshan-core-cf/pom.xml
+cp leshan/leshan-server-demo/pom.dev leshan/leshan-server-demo/pom.xml
+
+cd californium
+mvn eclipse:eclipse
+
+cd..
+cd leshan
+mvn eclipse:eclipse
